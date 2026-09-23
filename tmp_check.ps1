d4a938dbabc232c826f7b2bff38b09fbdfc77230
jinge5998 <shaojunking@163.com>
2026-09-23 22:29:03 +0800

    v1.0.2.1: 清理临时文件

--- tmp_check.ps1
+++
@@ -1,7 +0,0 @@
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-$procs = Get-Process | Where-Object { $_.ProcessName -like 'NetSecurityScanner*' -or $_.MainWindowTitle -like '*网络安全*' }
-if ($procs) {
-    $procs | Select-Object Id, ProcessName, StartTime, MainWindowTitle | Format-Table -AutoSize
-} else {
-    Write-Host "No process found"
-}
--- verify_publish.ps1
+++
@@ -1,6 +0,0 @@
-$dir = "d:\360安全浏览器下载\软件开发备份\网络安全漏洞扫描\NetSecurityScanner\publish-v1.0.1.5"
-$files = Get-ChildItem -Path $dir -File
-$matches = $files | Where-Object { $_.Name -match "LiveCharts|Skia|HarfBuzz|libSkia|libHarf" }
-$matches | ForEach-Object { Write-Host $_.Name }
-Write-Host "---"
-Write-Host "Total files: $($files.Count)"
